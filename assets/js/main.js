9382ff4ebe6cbb8ee113fa5f2c5c0e14ca5cfbab
Justin-Insight <jparsons@insightcreative.com>
2021-11-04 12:13:53 -0500

    updated mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,12 +32,6 @@ searchButton.addEventListener('click', event => {
     }
 })
 
-// 1. set mobile nav height to 0
-const mobileMenuHeight = mobileMenu.getBoundingClientRect().height
-
-mobileMenu.style.height = 0 
-// 2. on click and nav open set height to the original full height of the mobile navigation
-
 function initSlider() {
     const slider = document.querySelector('.slider')
     const slides = Array.from(slider.querySelectorAll('.slider__slide'))
@@ -108,6 +102,11 @@ window.addEventListener("scroll", function() {
 hamburger.addEventListener("click", toggleMobileMenu);
 
 function toggleMobileMenu() {
+    const mobileMenuWrapper = document.querySelector('.site-header__mobile-nav-inner')
+    const mobileMenuWrapperHeight = mobileMenuWrapper.getBoundingClientRect().height
+
+    mobileMenu.style.height = 0
+
     if(mobileMenu.classList.contains("nav-open")) {
         this.setAttribute("aria-expanded", "false");
         this.setAttribute("aria-label", "open mobile menu");
@@ -116,7 +115,7 @@ function toggleMobileMenu() {
         hamburger.classList.remove("is-active");
     } else {
         mobileMenu.classList.add("nav-open");
-        mobileMenu.style.height = mobileMenuHeight + 'px'
+        mobileMenu.style.height = mobileMenuWrapperHeight + 'px'
         hamburger.classList.add("is-active");
         this.setAttribute("aria-expanded","true");
         this.setAttribute("aria-label","close mobile menu");
